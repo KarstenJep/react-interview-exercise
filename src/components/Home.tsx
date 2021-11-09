17eee223f07d601f56df6bfa0cb076968956a1a5
Karsten Jepsen <kjepsen86@gmail.com>
2021-11-08 19:17:04 -0600

    Restructered layout. Imported districtList and SchoolList, added conditional rendering toggle

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -15,6 +15,7 @@ import {
     InputLeftElement,
     Stack,
     HStack,
+    Box,
     List,
     ListIcon,
     VStack,
@@ -24,190 +25,151 @@ import { Search2Icon } from '@chakra-ui/icons'
 import { Card } from '@components/design/Card'
 import { searchSchoolDistricts, searchSchools, NCESDistrictFeatureAttributes, NCESSchoolFeatureAttributes } from "@utils/nces"
 import Logo from '../logo192.png';
+import './SchoolList'
+import DistrictList from "./DistrictList"
+import SchoolList from "./SchoolList"
 
 
 const Home: React.FC = () => {
-    const [searching, setSearching] = React.useState(false);
-    const [userSearch, setUserSearch] = React.useState(false);
-    const [showDistrictList, setShowDistrictList] = React.useState(false);
-    const [showSchoolList, setShowSchoolList] = React.useState(false);
-    const [districtSearch, setDistrictSearch] = React.useState<NCESDistrictFeatureAttributes[]>([]);
-    const [districtSchoolSearch, setDistrictSchoolSearch] = React.useState<NCESSchoolFeatureAttributes[]>([]);
-    const [schoolSearch, setSchoolSearch] = React.useState<NCESSchoolFeatureAttributes[]>([]);
-    const [district, setDistrict] = React.useState('');
-    const [districtSchool, setDistrictSchool] = React.useState('');
-    const [school, setSchool] = React.useState('');
+
+    const [schoolForm, setSchoolForm] = React.useState(false);
+    console.log('schoolForm', schoolForm)
+    // const [searching, setSearching] = React.useState(false);
+    // const [userSearch, setUserSearch] = React.useState(false);
+    // const [showDistrictList, setShowDistrictList] = React.useState(false);
+    // const [showSchoolList, setShowSchoolList] = React.useState(false);
+    // const [districtSearch, setDistrictSearch] = React.useState<NCESDistrictFeatureAttributes[]>([]);
+    // const [districtSchoolSearch, setDistrictSchoolSearch] = React.useState<NCESSchoolFeatureAttributes[]>([]);
+    // const [schoolSearch, setSchoolSearch] = React.useState<NCESSchoolFeatureAttributes[]>([]);
+    // const [district, setDistrict] = React.useState('');
+    // const [districtSchool, setDistrictSchool] = React.useState('');
+    // const [school, setSchool] = React.useState('');
     
-    // Validate inputs
-    const validateDistrict = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        console.log('in validateDistrict', district);
-        setUserSearch(true);
-        setSearching(true);
-        setShowDistrictList(true);
-        handleDistrict();
+    // // Validate inputs
+    // const validateDistrict = (e: React.FormEvent<HTMLFormElement>) => {
+    //     e.preventDefault();
+    //     console.log('in validateDistrict', district);
+    //     setUserSearch(true);
+    //     setSearching(true);
+    //     // setShowDistrictList(true);
+    //     handleDistrict();
         
-    }
+    // }
 
-     // Validate inputs
-     const validateSchool = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        console.log('in validateDistrict', district);
-        setUserSearch(true);
-        setSearching(true)
-        handleSchool();
-    }
+    //  // Validate inputs
+    //  const validateSchool = (e: React.FormEvent<HTMLFormElement>) => {
+    //     e.preventDefault();
+    //     console.log('in validateDistrict', district);
+    //     setUserSearch(true);
+    //     setSearching(true)
+    //     handleSchool();
+    // }
 
-    // function to handle district and districtSchools search queries. 
-    const handleDistrict= async () => {
-        // console.log('in handleDistrict', district, districtSchool);
+    // // function to handle district and districtSchools search queries. 
+    // const handleDistrict= async () => {
+    //     // console.log('in handleDistrict', district, districtSchool);
         
-        const districtSearch = await searchSchoolDistricts(district)
-        setDistrictSearch(districtSearch)
+    //     const districtSearch = await searchSchoolDistricts(district)
+    //     setDistrictSearch(districtSearch)
        
-        console.log("District(s):", districtSearch);
+    //     console.log("District(s):", districtSearch);
     
-        // const districtSchoolSearch = await searchSchools(districtSchool, districtSearch[1].LEAID)
-        // setDistrictSchoolSearch(districtSchoolSearch)
-        // console.log("District School(s):", districtSchoolSearch);
-        setSearching(false)
-        console.log("district state", showDistrictList, "school state", showSchoolList)
-        //Clear form
-        setDistrict('')
+    //     // const districtSchoolSearch = await searchSchools(districtSchool, districtSearch[1].LEAID)
+    //     // setDistrictSchoolSearch(districtSchoolSearch)
+    //     // console.log("District School(s):", districtSchoolSearch);
+    //     setSearching(false)
+    //     // console.log("district state", showDistrictList, "school state", showSchoolList)
+    //     //Clear form
+    //     setDistrict('')
         
-    }
+    // }
 
-    // function to handle school search queries that bypass district
-    const handleSchool = async () => {
-        // console.log('in handleSchool', school);
-        setShowDistrictList(false);
-        const schoolSearch = await searchSchools(school)
-        setSchoolSearch(schoolSearch)
+    // // function to handle school search queries that bypass district
+    // const handleSchool = async () => {
+    //     // console.log('in handleSchool', school);
+    //     setShowDistrictList(false);
+    //     const schoolSearch = await searchSchools(school)
+    //     setSchoolSearch(schoolSearch)
        
-        console.log("School:", schoolSearch);
-        setSearching(false)
-        console.log("district state", showDistrictList, "school state", showSchoolList)
-        //Clear form
-        setSchool('')
-       
-    }
+    //     console.log("School:", schoolSearch);
+    //     setSearching(false)
+    //     console.log("district state", showDistrictList, "school state", showSchoolList)
+    //     //Clear form
+    //     setSchool('')
+    // }
 
-    // useEffect is listening, ready to call search function
-    useEffect(() => {
-        handleDistrict();
-        handleSchool();
-    }, [])
+    // // useEffect is listening, ready to call search function
+    // useEffect(() => {
+    //     handleDistrict();
+    //     // handleSchool();
+    // }, [])
     
     return (
         <Center padding="100px" > {/* Removed height="90vh" to allow for scrolling */}
             <ScaleFade initialScale={0.9} in={true}>
-                <Card variant="rounded" borderColor="green.400">
-                    <Heading>School Data Finder</Heading>
-
-                    <Divider margin={4} />
-                    
-                    {/* Hstack places forms side by side
-                    TO DO: add mobile/tablet responsiveness */}
-                    <HStack spacing={12}>
-                    {/* District Form */}
-                    <form onSubmit={validateDistrict}>
-                        <Stack spacing={3}>
-                            {/* Helper Text */}
-                            <Text textAlign="center">
-                                Enter a <b>District</b>
-                            </Text>
-                            {/* District Input*/}
-                            <InputGroup>
-                                <InputLeftElement
-                                pointerEvents="none"
-                                children={<Search2Icon color="blue.500" />}
-                                />
-                                <Input 
-                                borderRadius="20"
-                                boxShadow="base"
-                                placeholder="District"
-                                value={district}
-                                isRequired
-                                onChange={(e) => setDistrict(e.target.value)} 
-                                />
-                            </InputGroup>
-                            {/* Search District Button */}
+                <Card variant="rounded" borderColor="blue.300">
+                    {/* TO DO: add mobile/tablet responsiveness */}
+                    {schoolForm ? 
+                        <>
+                        <Heading color="yellow.800">School Data Finder</Heading>
+                        <Divider margin={3} />
+                        <HStack spacing={2}>
                             <Button 
                                 colorScheme="blue"
                                 boxShadow="md" 
-                                variant="solid"
-                                type="submit"
-                                isFullWidth>
-                                Search
+                                variant="ghost"
+                                onClick={() => setSchoolForm(false)}>
+                                Districts
                             </Button>
-                            <Text textAlign="center">
-                                {searching ? 
-                                <Spinner /> 
-                                : 
-                                <Text textAlign="center">{districtSearch.length} Districts</Text>
-                                }
-                            </Text>
-                        </Stack>
-                    </form>
-
-                    <Text>
-                        OR
-                    </Text>
-
-                    {/* School Form */}
-                    <form onSubmit={validateSchool}>
-                        <Stack spacing={3}>
-                            {/* Helper Text */}
-                            <Text textAlign="center">
-                                Enter a <b>School</b>
-                            </Text>
-                            {/* School Input*/}
-                            <InputGroup>
-                                <InputLeftElement
-                                    children={<Search2Icon color="yellow.500" />}
-                                />
-                                <Input 
-                                    focusBorderColor="yellow.500"
-                                    borderRadius="20"
-                                    boxShadow="base"
-                                    placeholder="School"
-                                    value={school}
-                                    isRequired
-                                    onChange={(e) => setSchool(e.target.value)}  
-                                />
-                            </InputGroup>
-                            {/* Search School Button */}
                             <Button 
                                 colorScheme="yellow"
                                 boxShadow="md" 
                                 variant="solid"
-                                type="submit">
-                                Search
+                                onClick={() => setSchoolForm(true)}>
+                                Schools
                             </Button>
-                            <Text textAlign="center">
-                                {searching ? 
-                                <Spinner /> 
-                                : 
-                                <Text>{schoolSearch.length} Schools</Text>
-                                }
-                            </Text>
-                        </Stack>
-                    </form>
-                    </HStack>
-
-                    <Divider margin={4} />
-                    
-                    {!userSearch && !showDistrictList &&
-                    <img width="100" src={Logo} alt="CharacterStrong Logo" />
+                        </HStack>
+                        <Divider margin={3} />
+                        <SchoolList />
+                        </>
+                        :
+                        <>
+                        <Heading color="blue.700">District Data Finder</Heading>
+                        <Divider margin={3} />
+                        <HStack spacing={2}>
+                            <Button 
+                                colorScheme="blue"
+                                boxShadow="md" 
+                                variant="solid"
+                                onClick={() => setSchoolForm(false)}>
+                                Districts
+                            </Button>
+                            <Button 
+                                colorScheme="yellow"
+                                boxShadow="md" 
+                                variant="ghost"
+                                onClick={() => setSchoolForm(true)}>
+                                Schools
+                            </Button>
+                        </HStack>
+                        <Divider margin={3} />
+                        <DistrictList />
+                        </>
                     }
+
+                    <Divider margin={3} />
+
+                    <img width="80vh" src={Logo} alt="CharacterStrong Logo" />
+                 
+                    
                     {/* UserSearch checks if the user has entered search query before mapping. This prevents mapping all unfiltered results on page load.
                     If user has not entered a search query, the number of total districts is displayed*/}
-                    {userSearch && showDistrictList ?
+                    {/* {userSearch && showDistrictList ?
                         <>
                             <Text><b><u>{district} - {districtSearch.length} Districts</u></b></Text>
-                            {districtSearch.map((districts) => {
-                            // console.log('In districtSearch map', districts);
-                                return (
+                            {districtSearch.map((districts) => { */}
+                          
+                                {/* return (
                                     <List spacing={3}>
                                         <ListItem
                                             key={districts.OBJECTID}
@@ -221,9 +183,9 @@ const Home: React.FC = () => {
                                     </List>
                                 )
                             })}
-                        </>
-                        :
-                        <>
+                        </> */}
+                        {/* : */}
+                        {/* <>
                             <Text><b><u>{school} - {schoolSearch.length} Schools</u></b></Text>
                             {schoolSearch.map((schools) => {
                             // console.log('In districtSearch map', districts);
@@ -241,7 +203,7 @@ const Home: React.FC = () => {
                                     </List>
                                 )
                             })} 
-                       </> }
+                       </> } */}
                  
                 </Card>
             </ScaleFade>
